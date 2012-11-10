f2d674651b8dcd54345fb487d500c13719ede980
Wojtek Siudzinski <admin@suda.pl>
2012-11-10 18:42:23 +0100

    Changed locale to english

--- openlaundryapi/settings.py
+++ openlaundryapi/settings.py
@@ -32,7 +32,7 @@ TIME_ZONE = 'Europe/Warsaw'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'pl'
+LANGUAGE_CODE = 'en-US'
 
 SITE_ID = 1
 
