b68c357d75ef9277ab3ca7b58f8431e7a830b7d7
Zbigniew Siciarz <antyqjon@gmail.com>
2012-11-10 19:42:06 +0100

    Automatic device creation.

--- users/models.py
+++ users/models.py
@@ -1,8 +1,11 @@
 
+from datetime import datetime
 import hashlib
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
+
+from devices.models import Device
 
 
 class UserProfile(models.Model):
@@ -16,9 +19,17 @@ def create_profile(sender, instance, created, **kwargs):
     Automatic profile creation when a User instance is created.
     """
     if created:
-        profile = UserProfile.objects.get_or_create(user=instance)
-        profile.token = hashlib.sha1("%s%s" % (datetime.now(), user.username)).hexdigest()
+        profile, created = UserProfile.objects.get_or_create(user=instance)
+        profile.token = hashlib.sha1("%s%s" % (datetime.now(), instance.username)).hexdigest()
         profile.save()
 
 
+def create_device(sender, instance, created, **kwargs):
+    if created:
+        device_id = hashlib.sha1("%s" % instance.id).hexdigest()
+        device = Device.objects.create(user=instance, device_id=device_id, name="%s's device" % instance.username)
+        device.save()
+
+
 post_save.connect(create_profile, sender=User, dispatch_uid='users.models.create_profile')
+post_save.connect(create_device, sender=User, dispatch_uid='users.models.create_device')
