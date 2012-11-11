3a474c86acd1216fafcb3ed1319ea40be1cfb3ea
Zbigniew Siciarz <antyqjon@gmail.com>
2012-11-11 09:38:43 +0100

    Redirect authenticated user to dashboard.

--- openlaundryapi/views.py
+++ openlaundryapi/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
+    if request.user.is_authenticated():
+        return redirect('users-dashboard')
     return render(request, "index.html", {
     })
